ac361b899eb361d8c989fba23425b4369ec14479
Zhaoxian Hu <huzhx@uci.edu>
2019-06-24 00:47:30 -0700

    write script to download dummy file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,22 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  fileTransferObject: FileTransferObject;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fileTransfer: FileTransfer,
+    private file: File
   ) {
     this.initializeApp();
   }
@@ -21,6 +27,20 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.fileTransferObject = this.fileTransfer.create();
+      this.downloadFiles();
     });
+  }
+
+  downloadFiles() {
+    const url = 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
+    this.fileTransferObject.download(url, this.file.externalDataDirectory + 'dummy.pdf').then(
+      entry => {
+        console.log('download complete: ' + entry.toURL());
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    File,
+    FileTransfer,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
